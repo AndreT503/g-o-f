9755b935871663b7c40175d596d69ad56da49a86
Andre Tournier <ATOURNIER3@msn.com>
2020-04-12 21:55:28 -0700

    b update
    synch v

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { AppModule } from './app/app.module';
-import { enableProdMode } from '@angular/core'
+import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
